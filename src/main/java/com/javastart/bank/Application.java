107a7a7d8722faec781c812f9d60f3bbc399afbd
Жаргал Тубденов <zhargal@Zhargal.sknt.ru>
2022-05-21 13:41:23 +0300

    Application updated

--- src/main/java/com/javastart/bank/Application.java
+++ src/main/java/com/javastart/bank/Application.java
@@ -3,15 +3,6 @@ package com.javastart.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Нужно написать приложение, которое будет создавать Account с параметрами пользователя
- * и счетом Bill
- *
- * Так же нужно будет уметь принимать платежи Payment и пополнение счета Adjustment
- *
- * И реализовать метод перевода денег с одного Account на другой (Transfer) в отдельном контроллере
- */
-
 @SpringBootApplication
 public class Application {
 
